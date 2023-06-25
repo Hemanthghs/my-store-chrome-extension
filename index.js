613e39ec0324cf2b69e83237cb21a77122167d82
hemanthghs <ghshemanth@gmail.com>
2023-06-25 19:41:10 +0530

    wip

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const mainEle = document.getElementById("main");
 const keyValEle = document.getElementsByClassName("key-value");
 const modeBtnEle = document.getElementById("mode");
+const bodyEle = document.getElementById('body');
+const myValEle = document.getElementsByClassName('my-value');
 
 modeBtnEle.addEventListener("change", function () {
   mainEle.classList.toggle("light-mode");
   mainEle.classList.toggle("dark-mode");
+  bodyEle.classList.toggle("light-mode");
+  bodyEle.classList.toggle("dark-mode");
   for (let i = 0; i < keyValEle.length; i += 1) {
     keyValEle.item(i).classList.toggle("key-value-light");
+    keyValEle.item(i).classList.toggle("tooltip-light");
   }
 });
+
+for (var i = 0; i < myValEle.length; i++) {
+  myValEle[i].onclick = function () {
+    const copyText = this.innerText;
+    navigator.clipboard.writeText(copyText);
+  }
+}
+
