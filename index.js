1a066477bf078b541955af0bd41737f8c99c31cb
hemanthghs <ghshemanth@gmail.com>
2023-06-25 18:00:08 +0530

    wip

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const modeEle = document.getElementById("mode");
 const mainEle = document.getElementById("main");
 const keyValEle = document.getElementsByClassName("key-value");
-const modeBtnEle = document.getElementById('mode');
+const modeBtnEle = document.getElementById("mode");
 
-modeBtnEle.addEventListener("change", function() {
-    if(modeEle.checked == true) {
-        if(mainEle.classList.contains('light-mode')) {
-            mainEle.classList.remove('light-mode');
-            mainEle.classList.add('dark-mode');
-            for(let ele in keyValEle) {
-                keyValEle[ele].classList.remove("key-value-light")
-            }
-        }
-    } else {
-        if(mainEle.classList.contains('dark-mode')) {
-            mainEle.classList.remove('dark-mode');
-            mainEle.classList.add('light-mode');
-            for(let ele in keyValEle) {
-                keyValEle[ele].classList.add("key-value-light")
-            }
-        }
-    }
-})
-
+modeBtnEle.addEventListener("change", function () {
+  mainEle.classList.toggle("light-mode");
+  mainEle.classList.toggle("dark-mode");
+  for (let i = 0; i < keyValEle.length; i += 1) {
+    keyValEle.item(i).classList.toggle("key-value-light");
+  }
+});
